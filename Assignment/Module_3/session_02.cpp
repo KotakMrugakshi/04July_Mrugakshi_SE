a02a6dd5b48463502b8d7d0334d86bc02f4e24c7
KotakMrugakshi <114981218+KotakMrugakshi@users.noreply.github.com>
2026-09-19 17:42:29 +0530

    Add files via upload

--- Assignment/Module_3/session_02.cpp
+++ Assignment/Module_3/session_02.cpp
@@ -1,17 +1,14 @@
 //Session 02
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Note
-{
+class Note{
 private:
-    string title;
-    string date;
-    bool isDone;
+    string title,date;
+    string isDone;
 
 public:
-    void setNote(string t, string d, bool status){
+    void setNote(string t, string d, string status){
         title = t;
         date = d;
         isDone = status;
@@ -26,16 +23,22 @@ public:
 
 main(){
 	string title,dat;
-	bool isDone;
+	string isDone;
+	char isStatus;
 	cout<<"Enter your title:";
 	cin>>title;
 	cout<<"\nEnter date:";
 	cin>>dat;
 	cout<<"\nFalse Press f and True press t";
 	cout<<"\nIsDone:";
-	cin>>isDone;
+	cin>>isStatus;
     Note n1;
     //n1.setNote("Tops", "18-09-2026", false);
+    if(isStatus=='t'||isStatus=='T'){
+    	isDone ="true";
+	}else{
+		isDone="false";
+	}
     n1.setNote(title, dat,isDone);
     n1.displayNote();
 }
